6eac3be782360e1e321825efc04813b82fbce158
Leroy Brown <leroybrown247@gmail.com>
2023-12-01 11:07:48 +1300

    Jamn:  Update Track.js to Include a Remove Button

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,21 +1,31 @@
-import React from "react"
-import "./Track.css"
+import React from "react";
+import "./Track.css";
 
 class Track extends React.Component {
-    render() {
-      const { name, artist, album } = this.props.track; // Destructuring
+  // define a method called renderAction that displays a - anchor tag if the isRemoval property is true, and a + anchor tag if the isRemoval property is false.
+  handleRemove = () => {
+    const { track, onRemove } = this.props;
+    // Pass the track property to the onRemove method
+    onRemove(track);
+  };
 
-      return (
-        <div className="Track">
-            <div className="Track-information">
-                <h3>{name}</h3>
-                {/*Track information*/}
-                <p>{artist} | {album}</p>
-                </div>
-          <button className="Track-action">+ or - Button</button>
+  render() {
+    const { name, artist, album } = this.props.track;
+
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{name}</h3>
+          {/*Track information*/}
+          <p>
+            {artist} | {album}
+          </p>
+        </div>
+        {/*Render a + or - anchor tag depending on the isRemoval property*/}
+        <button className="Track-action" onClick={this.handleRemove}>-</button>
       </div>
-      )
-    }
+    );
   }
-  
+}
+
 export default Track;
