94f32ed395c04b3adc49eccd9fd75f2528120a43
Leroy Brown <leroybrown247@gmail.com>
2023-11-30 20:53:51 +1300

    Jamn: SearchResults in App component passes down state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,27 @@ import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import "./App.css";
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Initial state of the searchResults array is set to an array of three objects
+    this.state = {
+      searchResults: [
+        { name: "name1", artist: "artist1", album: "album1", id: 1 },
+        { name: "name2", artist: "artist2", album: "album2", id: 2 },
+        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        // Hardcoded values for the playlistName and playlistTracks states
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
         <h1>JAMN!</h1>
         <SearchBar />
         <header className="App-playlist"></header>
-        <SearchResults />
+        {/*Pass the state of searchResults down to the SearchResults component*/}
+        <SearchResults searchResults={this.state.searchResults} />
         <Playlist />
       </div>
     );
