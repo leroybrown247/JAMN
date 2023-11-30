01d3578eae67c2be3a24f06a43b47e27ba9aa747
Leroy Brown <leroybrown247@gmail.com>
2023-11-30 21:03:35 +1300

    Jamn: Track component renders information

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,12 +3,14 @@ import "./Track.css"
 
 class Track extends React.Component {
     render() {
+      const { name, artist, album } = this.props.track; // Destructuring
+
       return (
         <div className="Track">
             <div className="Track-information">
-                <h3>Track Name</h3>
+                <h3>{name}</h3>
                 {/*Track information*/}
-                <p>Artist | Album</p>
+                <p>{artist} | {album}</p>
                 </div>
           <button className="Track-action">+ or - Button</button>
       </div>
