298cfebb3232ac7a9f69ee818c3610fb70d61b7c
Leroy Brown <leroybrown247@gmail.com>
2023-11-30 19:58:53 +1300

    Jamn: SearchBar CSS updated

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,10 +3,18 @@ import "./SearchBar.css";
 
 class SearchBar extends React.Component {
   render() {
-    return <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+    return ( 
+    <div className="SearchBarContainer">       
+      <div className="SearchBar">
+        <input className="searchInput" placeholder="Enter A Song, Album, or Artist" />
+        <div className="iconContainer">
+
         <button className="SearchButton">SEARCH</button>
+        </div>
+      </div>
     </div>
+    )
+    
   }
 }
 
