64fea3672f61e5b8eb32999e6f433d588249f156
Leroy Brown <leroybrown247@gmail.com>
2023-12-02 18:23:15 +1300

    Jamn: Playlist.js includes Playlist renaming

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -3,32 +3,73 @@ import Tracklist from "../Tracklist/Tracklist";
 import "./Playlist.css";
 
 class Playlist extends React.Component {
-  // Pass the track property to the onRemove method
-  handleRemove = (track) => {
-    const { playlistTracks, onRemove } = this.props;
 
-    // Check if the track is in the playlistTracks state
-    const updatedPlaylist = playlistTracks.filter(
-      (playlistTrack) => playlistTrack.id !== track.id
-    );
-    // Update the playlistTracks state
+  // Define a state for the playlistName
 
-    onRemove(track);
+  state = {
+    isEditing: false,
   };
+
+  // Define a method to toggle the isEditing state
+
+  toggleEdit = () => {
+    this.setState((prevState) => ({
+      isEditing: !prevState.isEditing,
+    }));
+  }
+
+  // Define a method to handle the playlistName change
+
+  handleNameChange = (event) => {
+    const { onNameChange } = this.props;
+
+    // Update the playlistName state
+    onNameChange(event.target.value);
+  }
+
+
+  // handleRemove = (track) => {
+  //   const { playlistTracks, onRemove } = this.props;
+
+  //   // Check if the track is in the playlistTracks state
+  //   const updatedPlaylist = playlistTracks.filter(
+  //     (playlistTrack) => playlistTrack.id !== track.id
+  //   );
+  //   // Update the playlistTracks state
+  //   onRemove(updatedPlaylist);
+  // };
 
   render() {
     const { playlistName, playlistTracks } = this.props;
+    const { isEditing } = this.state;
 
     return (
       <div className="playlist-container">
         <div className="playlist">
-            <input className="playlistInput" defaultValue={playlistName} />
-            {/*Pass the onRemove method to the Tracklist component*/}
+          {/* Render the playlistName as an input if editing is true */}
+
+          {isEditing ? (
+            
+            <input 
+            className="playlistInput" 
+            defaultValue={playlistName} 
+            onBlur={this.toggleEdit}
+            onChange={this.handleNameChange}
+            />
+
+          ) : (
+              <h2 className="playlistTitle" onClick={this.toggleEdit}>
+                {playlistName}
+              </h2>
+            )}
+
             <Tracklist tracks={playlistTracks} onRemove={this.handleRemove} />
+
           <button className="Playlist-btn">SAVE</button>
+          
         </div>
       </div>
-    );
+    )
   }
 }
 
