14bc5176e2db82fb607e5ef2c0110724b4abdd68
Leroy Brown <leroybrown247@gmail.com>
2023-12-03 23:35:14 +1300

    Refactored SearchBar.js to useState hook to manage state

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,67 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 import Spotify from "../../Utils/Spotify";
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: "",
-    };
-  }
+function SearchBar({ onSearchResults }) {
+  const [term, setTerm] = useState("");
 
-    handleTermChange = (event) => {
-
-const newTerm = event.target.value;
-if (newTerm !== this.state.term) {
-
-      this.setState({
-        term: newTerm,
-      });
-    };
+  const handleTermChange = (event) => {
+    const newTerm = event.target.value;
+    if (newTerm !== term) {
+      setTerm(newTerm);
+    }
   };
 
-    handleSearch = () => {
-      console.log('Search button clicked');
-      const { term } = this.state;
+  const handleSearch = () => {
+    console.log('Search button clicked');
 
-      if (term.trim() !== "") {
+    if (term.trim() !== "") {
+      Spotify.search(term)
+        .then((searchResults) => {
+          console.log('Spotify.search results:', searchResults);
+          setTerm(searchResults);
+          onSearchResults(searchResults);
+        })
+        .catch((error) => {
+          console.log("Error searching for tracks:", error.message);
+        });
+    } else {
+      console.log("Please enter a search term.");
+    }
+  };
 
-        Spotify.search(term)
-  .then((searchResults) => {
-    console.log('Spotify.search results:', searchResults);
-    this.setState({ term: searchResults }, () => {
-      this.props.onSearchResults(this.state.term);
-    });
-
-  })
-  .catch((error) => {
-    console.log("Error searching for tracks:", error.message);
-  });
-
-      } else {
-        console.log("Please enter a search term.");
-      }
-    };
-
-  render() {
-    console.log('Rendering search results');
-    return (
+  console.log('Rendering search results');
+  return (
       <div className="SearchBarContainer">
         <div className="SearchBar">
           <input
             className="searchInput"
             placeholder="Enter A Song, Album, or Artist"
-            onChange={this.handleTermChange}
+            onChange={handleTermChange}
           />
 
-          <button className="SearchButton" onClick={this.handleSearch}>
+          <button className="SearchButton" onClick={handleSearch}>
             SEARCH
           </button>
         </div>
       </div>
     );
   }
-}
+
 
 export default SearchBar;
