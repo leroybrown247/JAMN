bdfc070008333f511a54efb30b2050421aa045a6
Leroy Brown <leroybrown247@gmail.com>
2023-12-03 14:19:01 +1300

    Extract access token and experation time

--- src/Utils/Spotify.js
+++ src/Utils/Spotify.js
@@ -5,12 +5,22 @@ const clientId = 'YOUR_SPOTIFY_CLIENT_ID';
 const redirectUri = 'YOUR_REDIRECT_URI';
 
 const Spotify = {
-    
+
   // Check if there is an access token in the URL
   getAccessToken() {
     if (accessToken) {
       return accessToken;
     }
+
+// Extract access token and expiration time from the URL
+const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+if (accessTokenMatch && expiresInMatch) {
+  accessToken = accessTokenMatch[1];
+  expiresIn = Number(expiresInMatch[1]);
+}
+
 }
 }
 
