1e1b729d15ee47fea6c3f21a9d1f06cc7b1e974d
Leroy Brown <leroybrown247@gmail.com>
2023-12-03 15:38:52 +1300

    SearchBar.js started connecting search bar to spotify

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,32 @@
 import React from "react";
 import "./SearchBar.css";
+import Spotify from "../../Utils/Spotify";
 
 class SearchBar extends React.Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    term: ""
+  }
+
+  // Define the method to handle user input
+
+  handleTermChange = (event) => {
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+// Define a method to handle the search button click
+
+handleSearch = () => {
+  const { term } = this.state;
+
+
+
+}
+
   render() {
     return ( 
     <div className="SearchBarContainer">       
