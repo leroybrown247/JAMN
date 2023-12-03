724b18936c850102969e4a951ab85405b62b3675
Leroy Brown <leroybrown247@gmail.com>
2023-12-03 14:15:18 +1300

    Set up a Spotify.js to handle API logic

---
+++ src/Utils/Spotify.js
@@ -0,0 +1,17 @@
+let accessToken;
+let expiresIn;
+
+const clientId = 'YOUR_SPOTIFY_CLIENT_ID';
+const redirectUri = 'YOUR_REDIRECT_URI';
+
+const Spotify = {
+    
+  // Check if there is an access token in the URL
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+}
+}
+
+export default Spotify;
